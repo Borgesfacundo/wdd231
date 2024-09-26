5d4f984e092a3f9e769cbc6b774881628e918447
Borgesfacundo <borgesfacundo@gmail.com>
2024-09-25 21:13:24 -0300

    asdasd

--- week02/scripts/scripts.js
+++ week02/scripts/scripts.js
@@ -18,10 +18,34 @@ getPokemon(url);
 console.log("second: ", results);*/
 
 const url = "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
-const cards = document.querySelector("#cards");
+const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.prophets);
+    //console.table(data.prophets); 
+    displayProphets(data.prophets);
 }
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement("section");
+        let fullName = document.createElement("h2");
+        let portrait = document.createElement("img");
+
+        fullName.innerHTML = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
