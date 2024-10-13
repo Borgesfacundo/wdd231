4b8374cb66a334c7de20f84a5164a20677b56734
Borgesfacundo <borgesfacundo@gmail.com>
2024-10-13 17:09:48 -0300

    asdasd

--- chamber/scripts/modals.js
+++ chamber/scripts/modals.js
@@ -4,14 +4,43 @@ const openModalBronze = document.querySelector(".bronze");
 const openModalSilver = document.querySelector(".silver");
 const openModalGold = document.querySelector(".gold");
 
-function displayMembershipDetails(membership) {
-    membershipDetails.innerHTML = "";
-    membershipDetails.innerHTML = `
-    <button id="closeModal">❌</button>
-    <h2>${membership.title}
-    <p>${membership.description}</p>
-    <p>Cost: ${membership.cost}</p>
-    `;
+const linkURL = "https://borgesfacundo.github.io/wdd231/chamber/data/membershiplevel.json";
+
+async function getMembershipLevel() {
+    const response = await fetch(linkURL);
+    const data = await response.json();
+
+    openModalNP.addEventListener("click",
+        displayMembershipDetails(data, openModalNP.classList));
+    openModalBronze.addEventListener("click",
+        displayMembershipDetails(data, openModalBronze.classList));
+    openModalSilver.addEventListener("click",
+        displayMembershipDetails(data, openModalSilver.classList));
+    openModalGold.addEventListener("click",
+        displayMembershipDetails(data, openModalGold.classList));
+
+};
+
+function displayMembershipDetails(membership, className) {
+    membership.forEach(lvl => {
+        if (lvl === className) {
+            membershipDetails.innerHTML = "";
+            membershipDetails.innerHTML = `
+            <button id="closeModal">❌</button>
+            <h2>${membership.title}
+            <p>${membership.description}</p>
+            <p>Cost: ${membership.cost}</p>
+            `;
+            modal.showModal();
+
+            closeModal.addEventListener("click", () => {
+                membershipDetails.close();
+            });
+        } else {
+
+        }
+
+    });
 
 }
 
